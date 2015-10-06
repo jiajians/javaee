88e8e0b79b4c66dc5b32b1cdd4464bd7e84b0eba
jiajians <song_jiajian@yahoo.com>
2015-10-06 14:57:20 -0300

    aaaaa

--- aaa/src/ca/jsong/dev/TestGit.java
+++ aaa/src/ca/jsong/dev/TestGit.java
@@ -1,4 +1,4 @@
-package ca.js.test;
+package ca.jsong.dev;
 
 public class TestGit {
 
@@ -6,7 +6,11 @@ public class TestGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
         System.out.println("I am from RAD 9.1.");
+        System.out.println("I am from RAD 9.1. " + "From EGit Plugin");
+        System.out.println("I am from RAD 9.1. " + "From EGit Plugin");
+        System.out.println("I am from RAD 9.1. " + "From EGit Plugin");
         System.out.println("I am from RAD 9.1. " + "From EGit Plugin");
                 
 	}
